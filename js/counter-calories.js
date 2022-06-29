c67790f75b482dfd2f79856b08af115b184c7144
Ekaterina <katemars18@gmail.com>
2022-06-29 20:52:22 +0500

    start project

---
+++ js/counter-calories.js
@@ -0,0 +1,20 @@
+const ActivityRatio = {
+    Min: 1.2,
+    Low: 1.375,
+    Medium: 1.55,
+    High: 1.725,
+    VeryHigh: 1.9
+}
+
+const PercentOfNormal = {
+    Min: 0.85,
+    Max: 1.15
+}
+
+const FormulaNumbers = {
+    Weight: 10,
+    Height: 6.25,
+    Age: 5,
+    Female: 161,
+    Male: 5
+}
