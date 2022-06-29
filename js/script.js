350644bfe7fa4a9c4da7d9cb587a7349751dccff
Ekaterina <katemars18@gmail.com>
2022-06-29 23:01:26 +0500

    update js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 import {formatInput} from '..js\input.js';
+import {formatNumber} from '..js\numbers.js'
 
 
 const counterItems = document.querySelectorAll('.counter')
+
+counterItems.forEach((item) => {
+    const counter = new Counter(item);
+    return counter.init();
+})
