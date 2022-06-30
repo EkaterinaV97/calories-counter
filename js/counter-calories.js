e563077cfe4b8bb66d587a36c1ee44106262b180
Ekaterina <katemars18@gmail.com>
2022-06-30 22:19:31 +0500

    update js

--- js/counter-calories.js
+++ js/counter-calories.js
@@ -1,3 +1,5 @@
+import { formatInput } from "./input";
+
 const ActivityRatio = {
     Min: 1.2,
     Low: 1.375,
@@ -17,4 +19,36 @@ const FormulaNumbers = {
     Age: 5,
     Female: 161,
     Male: 5
+}
+
+export default class Counter {
+    constructor(count) {
+        this.module = count;
+        this.form = this.module.querySelector('.counter__form');                         /* from HTML class .counter-form.  */
+        this.modules = this.form.modules;
+        this.parameters = this.modules.parameters;
+
+        this.age = this.modules.age;
+        this.weight = this.modules.weight;
+        this.gender = this.modules.gender;                          /* parameters for formula */
+        this.height = this.modules.height;
+        this.activity = this.modules.activity;
+
+        this.show = this.modules.show;
+        this.hide = this.module.hide;                                /* show or hide element */
+
+        this.result = new Result(this.module)
+
+    }
+
+    CaloriesNorm() {
+
+    }
+
+    InputForm() {
+        this.age.value = formatInput(this.age);
+        this.weight.value = formatInput(this.weight);
+        this.height.value = formatInput(this.height);
+        
+    }
 }
